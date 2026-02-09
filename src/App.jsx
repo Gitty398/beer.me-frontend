0334291e3cfaf5fe300bddf784648a398230465e
Thomas Phillips <fatesunited@gmail.com>
2026-02-09 14:16:54 -0600

    BeerList v0, also moved components folder to src and added skeleton code to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
 import './App.css'
+import { useContext, useState, useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router';
+// import * as beerService from './services/beerService';
+// import Navbar from './components/Navbar/Navbar';
+// import SignUpForm from './components/SignUpForm/SignUpForm';
+// import SignInForm from './components/SignInForm/SignInForm';
+// import LandingPage from './components/LandingPage/LandingPage';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import BeerList from './pages/BeerList/BeerList';
+// import BeerDetail from './pages/BeerDetail/BeerDetail';
+// import BeerForm from './pages/BeerForm/BeerForm';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [beers, setBeers] = useState([]);
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <>
-
+      {/* <Navbar />
+      <Routes>
+        
+      </Routes> */}
+      <h1>Hello world!</h1>
     </>
   )
 }
--- src/pages/BeerList/BeerList.jsx
+++ src/pages/BeerList/BeerList.jsx
@@ -1,1 +1,26 @@
 // Thomas
+
+import { Link } from 'react-router';
+
+const BeerList = (props) => {
+  return (
+    <main>
+      {props.beers.map((beer) => (
+        <Link key={beer._id} to={`/beers/${beer._id}`}>
+          <article>
+            <header>
+              <h2>{beer.name}</h2>
+              <p>
+                {`${beer.owner.username} posted on
+                ${new Date(beer.createdAt).toLocaleDateString()}`}
+              </p>
+            </header>
+            <p>{beer.location.address}</p>
+          </article>
+        </Link>
+      ))}
+    </main>
+  );
+};
+
+export default BeerList;
