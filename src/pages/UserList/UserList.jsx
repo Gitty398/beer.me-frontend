e042287f976fa976a79974c3037d0ada26874334
John F <anabasisinc@outlook.com>
2026-02-11 16:06:05 -0500

    fixed bug so the logged in user doesnt show

--- src/pages/UserList/UserList.jsx
+++ src/pages/UserList/UserList.jsx
@@ -1,9 +1,17 @@
-import { Link } from 'react-router';
 
-const UserList = ({ users }) => {
+import { Link, useParams } from 'react-router';
+import {useContext} from "react"
+import {UserContext} from "../../contexts/UserContext"
+
+const UserList = ({users}) => {
+    const { user } = useContext(UserContext)
+
+    const filteredUsers = users.filter((u)=> String(u._id) !== String(user._id))
+    
     return (
         <main className="beer-card">
-            {users.map((u) => (
+           
+            {filteredUsers.map((u) => (
                 <Link key={u._id} to={`/users/${u._id}/beer`}>
                     <article>
                         <header>
