753c6b952460b22b1298caa556fac23764455cab
Thomas Phillips <fatesunited@gmail.com>
2026-02-12 15:38:19 -0600

    added header for all beers

--- src/pages/BeerList/BeerList.jsx
+++ src/pages/BeerList/BeerList.jsx
@@ -3,6 +3,7 @@ import BeerCard from "../../components/BeerCard/BeerCard";
 const BeerList = ({beers}) => {
   return (
     <main>
+      <h2>All Beers</h2>
       <ul>        
         {beers.map((beer) => (
           <li key={beer._id}>
