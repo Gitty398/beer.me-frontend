fe00a8756c6c535868971fdf92b73a473067eba2
Thomas Phillips <fatesunited@gmail.com>
2026-02-12 12:35:13 -0600

    added beercount to location form and location card

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -1,5 +1,3 @@
-
-
 const LocationCard = ({
     loc,
     onDeleteLocation,
@@ -12,15 +10,15 @@ const LocationCard = ({
         <div className="location-card" key={loc._id}>
             <h3>{loc.name}</h3>
             <p>{loc.address}</p>
+            <p>Number of Beers: {loc.beerCount}</p>
             <p>Price: {loc.beerPrice}</p>
             <p>{loc.beerRating} ⭐</p>
             <p>Notes: {loc.notes}</p>
             <p>Last Updated on {new Date(loc.createdAt).toLocaleDateString()}</p>
             {beer.owner._id === user._id && (
                 <>
-                {console.log(beer.owner)}
-                <button onClick={() => handleEditLocationButton(beer._id, loc._id)}>Edit Location</button>
-                <button onClick={() => onDeleteLocation(loc._id)}>Delete Location</button>
+                    <button onClick={() => handleEditLocationButton(beer._id, loc._id)}>Edit Location</button>
+                    <button onClick={() => onDeleteLocation(loc._id)}>Delete Location</button>
                 </>
              )} 
         </div>
